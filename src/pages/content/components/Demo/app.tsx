d2385f39c82663689f1790f0cd0bee3e3058394e
Alexey Ardov <ardovalexey@gmail.com>
2023-08-12 00:30:44 +0200

    apply styles

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,54 +1,54 @@
-const footer = document.getElementsByClassName("srf-layout__footer")[0];
-const sidebar = document.getElementsByClassName("srf-layout__sidebar")[0];
+const footer = document.getElementsByClassName('srf-layout__footer')[0]
+const sidebar = document.getElementsByClassName('srf-layout__sidebar')[0]
 
 const toggleFooter = () => {
-  if (footer.style.display === "none") {
-    footer.style.display = "block";
-    chrome.storage.local.set({ hideFooter: false });
+  if (footer.style.display === 'none') {
+    footer.style.display = 'block'
+    chrome.storage.local.set({ hideFooter: false })
   } else {
-    footer.style.display = "none";
-    chrome.storage.local.set({ hideFooter: true });
+    footer.style.display = 'none'
+    chrome.storage.local.set({ hideFooter: true })
   }
-};
+}
 
 const toggleSidebar = () => {
-  if (sidebar.style.display === "none") {
-    sidebar.style.display = "block";
-    chrome.storage.local.set({ hideSidebar: false });
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block'
+    chrome.storage.local.set({ hideSidebar: false })
   } else {
-    sidebar.style.display = "none";
-    chrome.storage.local.set({ hideSidebar: true });
+    sidebar.style.display = 'none'
+    chrome.storage.local.set({ hideSidebar: true })
   }
-};
+}
 
 async function init() {
-  const values = await chrome.storage.local.get(["hideFooter", "hideSidebar"]);
+  const values = await chrome.storage.local.get(['hideFooter', 'hideSidebar'])
 
-  console.log(values);
+  console.log(values)
 
-  footer.style.display = values.hideFooter ? "none" : "block";
-  sidebar.style.display = values.hideSidebar ? "none" : "block";
+  footer.style.display = values.hideFooter ? 'none' : 'block'
+  sidebar.style.display = values.hideSidebar ? 'none' : 'block'
 }
 
 export default function App() {
-  init();
+  init()
   return (
     <>
       {/* <div className="content-view text-lime-400">content view</div> */}
       <div
         style={{
-          position: "fixed",
+          position: 'fixed',
           zIndex: 9999,
           top: 8,
           right: 8,
           width: 200,
           height: 100,
-          background: "white",
-          border: "1px solid #aaa",
+          background: 'white',
+          border: '1px solid #aaa',
           borderRadius: 8,
           padding: 16,
-          display: "flex",
-          flexDirection: "column",
+          display: 'flex',
+          flexDirection: 'column',
           gap: 8,
         }}
       >
@@ -56,5 +56,5 @@ export default function App() {
         <button onClick={toggleSidebar}>Hide left menu</button>
       </div>
     </>
-  );
+  )
 }
--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "@src/pages/content/components/Demo/app";
-import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { createRoot } from 'react-dom/client'
+import App from '@src/pages/content/components/Demo/app'
+import refreshOnUpdate from 'virtual:reload-on-update-in-view'
 
-refreshOnUpdate("pages/content");
+refreshOnUpdate('pages/content')
 
-const root = document.createElement("div");
-root.id = "sem-nav-extension-root";
-document.body.append(root);
+const root = document.createElement('div')
+root.id = 'sem-nav-extension-root'
+document.body.append(root)
 
-createRoot(root).render(<App />);
+createRoot(root).render(<App />)
